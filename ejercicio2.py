cdea4416d834e822891c27b7489207abcdf3cfe8
joakoCARP <joaquinalvarez.et32@gmail.com>
2025-07-11 20:47:11 -0300

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,6 @@
+numero = int(input("ingrese un numero"))
+if numero % 2 == 0:
+    print("el numero es par")
+else:
+    print("el numero es impar")
+    print(f"su valor al cuadrado es {numero ** 2}")
