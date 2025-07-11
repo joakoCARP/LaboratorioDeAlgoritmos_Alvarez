97114ca01b6d799fe40d0128a0684c129956862a
joakoCARP <joaquinalvarez.et32@gmail.com>
2025-07-11 14:41:39 -0300

    Create ejercicio8.py

---
+++ ejercicio8.py
@@ -0,0 +1,5 @@
+contraseña = input("ingrese la contraseña")
+if contraseña == "python123":
+    print("contraseña correcta! Acceso concedido")
+else:
+    print("contraseña incorrecta, autodestruccion en 5 minutos....")
