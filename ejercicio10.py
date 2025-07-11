62ceeef7b17c8af0a0159320fb249179b03ccc18
joakoCARP <joaquinalvarez.et32@gmail.com>
2025-07-11 14:42:16 -0300

    Create ejercicio10.py

---
+++ ejercicio10.py
@@ -0,0 +1,6 @@
+edad = int(input("cual es tu edad?"))
+pochoclos = input("compraste pochoclos? (si/no): ")
+if edad > 65 and pochoclos == "si":
+    print("felicidades! tenes entrada gratuita al cine")
+else:
+    print("ahora compras la entrada, o si no te cago a patadas de aca")
