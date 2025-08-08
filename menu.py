71568208c3b34c84c4eac6d62c6c02986e009ee2
joakoCARP <joaquinalvarez.et32@gmail.com>
2025-08-08 15:06:06 -0300

    menu.py

--- menu.py
+++ menu.py
@@ -1,13 +1,14 @@
 def dividir():
     try:
-    num1 = float(input("ingresa el primer numero"))
-    num2 = float(input("ingresa el segundo numero"))
-    resultado = num1 / num2
-    print(f"el resultado es {resultado}")
-except ZeroDivisionError:
-    print("no se puede dividir por cero")
-except ValueError:
-    print("ingresa numeros validos")
+        num1 = float(input("ingresa el primer numero"))
+        num2 = float(input("ingresa el segundo numero"))
+        resultado = num1 / num2
+        print(f"el resultado es {resultado}")
+    except ZeroDivisionError:
+        print("no se puede dividir por cero")
+    except ValueError:
+        print("ingresa numeros validos")
+dividir()
 
 def edad():
     while True:
