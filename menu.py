92ae939f4f9c7c38b64eb9acaebe7007cd521c57
joakoCARP <joaquinalvarez.et32@gmail.com>
2025-08-08 15:36:25 -0300

    menu.py

--- menu.py
+++ menu.py
@@ -11,46 +11,38 @@ def dividir():
         print("no se puede dividir por cero")
     except ValueError:
         print("ingresa numeros validos")
-dividir()
 
 def edad():
     while True:
-    try:
-        edad = int(input("ingresa tu edad"))
-        print("tenes {edad} años")
-        break
-    except ValueError:
-        print("edad no identificadad, intente de nuevo")
-edad()
+        try:
+            edad = int(input("ingresa tu edad"))
+            print(f"tenes {edad} años")
+            break
+        except ValueError:
+            print("edad no identificada, intenta de nuevo")
 
 nombres = ["Ana", "Pedro", "Sofía"]
 
 def nombreIndice():
     while True:
         try:
-            indice = int(input("ingresa un número indice (0 a 2)"))
-            print(f(el nombre en el índice es: {nombres[indice]}")
+            indice = int(input("ingresa un numero indice (0 a 2)"))
+            print(f"el nombre en el índice es: {nombres[indice]}")
             break
         except ValueError:
             print("numero invaido. Intenta con un numero entero")
         except IndexError:
             print("indice fuera del rango. Proba con 0, 1 o 2.")
 
-nombreIndice()
-
 def numerosEnteros():
     try:
         num1 = int(input("ingresa el primer numero"))
         num2 = int(input("ingresa el segundo numero"))
         resultado = num1 + num2
-        print(f(la suma es: {resultado}")
+        print(f"la suma es: {resultado}")
     except (ValueError, TypeError):
         print("error. No se puede ingresar texto o numeros con texto.")
 
-
-numerosEnteros()
-
-            
 def programaDivision():
     try:
         num1 = float(input("ingresa el primer numero"))
@@ -63,8 +55,6 @@ def programaDivision():
         print("no pongas letras")
     finally:
         print("Fin del programa de cálculo.")
-
-programaDivision()
 
 def mostrarMenu():
     print("\nmenu de ejercicios:")
@@ -93,3 +83,5 @@ while True:
         break
     else:
         print("Opcion invalida. Proba de nuevo.")
+
+mostrarMenu()
