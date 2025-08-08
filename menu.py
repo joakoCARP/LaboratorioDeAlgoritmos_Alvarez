c2d7f2fa00887cc415533736345a95f08fb4d370
joakoCARP <joaquinalvarez.et32@gmail.com>
2025-08-08 14:44:03 -0300

    menu.py

--- menu.py
+++ menu.py
@@ -1,10 +1,10 @@
-def.dividir():
+def dividir():
     try:
-    num1.=.float(input("ingresa.el.primer.numero"))
-    num2.=.float(input("ingresa.el.segundo.numero"))
-    resultado.=.num1./.num2
-    print(f"el.resultado.es.{resultado}")
-except.ZeroDivisionError:
-    print("nosepuededividirporcero")
-except.ValueError:
-    print("ingresanumerosvalidos")
+    num1 = float(input("ingresa el primer numero"))
+    num2 = float(input("ingresa el segundo numero"))
+    resultado = num1 / num2
+    print(f"el resultado es {resultado}")
+except ZeroDivisionError:
+    print("no se puede dividir por cero")
+except ValueError:
+    print("ingresa numeros validos")
