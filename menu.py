d443b167bcd2229e22f21143c4400ae3955acc06
joakoCARP <joaquinalvarez.et32@gmail.com>
2025-08-08 14:41:44 -0300

    menu.py

---
+++ menu.py
@@ -0,0 +1,10 @@
+def.dividir():
+    try:
+    num1.=.float(input("ingresa.el.primer.numero"))
+    num2.=.float(input("ingresa.el.segundo.numero"))
+    resultado.=.num1./.num2
+    print(f"el.resultado.es.{resultado}")
+except.ZeroDivisionError:
+    print("nosepuededividirporcero")
+except.ValueError:
+    print("ingresanumerosvalidos")
