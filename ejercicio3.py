7c10bc14263724c27ce4b450845bf2618e4b7760
joakoCARP <joaquinalvarez.et32@gmail.com>
2025-08-15 21:03:25 -0300

    ejercicio3.py

---
+++ ejercicio3.py
@@ -0,0 +1,9 @@
+lista1 = [5, 1, 8]
+lista2 = [3, 9, 2]
+listacombinada = lista1 + lista2
+
+valortotal = 0
+for numero in listacombinada:
+    valortotal += numero
+    print(valortotal)
+valortotal
