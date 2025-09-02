ddac1fd98c7bfc2dc1106a9dde837d15f40fb590
joakoCARP <joaquinalvarez.et32@gmail.com>
2025-09-02 13:43:32 -0300

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,1 +1,10 @@
+matriz = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 15, 20],
+    [25, 30, 35, 40]
+]
 
+numerogrande = max(max(fila) for fila in matriz)
+
+print(f"El número mas grande de la matriz es {numerogrande}")
