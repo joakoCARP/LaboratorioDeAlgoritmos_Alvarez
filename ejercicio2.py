992bc163cead6856299c67cc6cd5eaddfad24366
joakoCARP <joaquinalvarez.et32@gmail.com>
2025-09-02 13:39:51 -0300

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,7 +1,9 @@
-matriz = [[16, 26, 76], 
-          [23, 56, 64],
-          [34, 17, 90]]
+matriz = [
+    [23, 43, 74],
+    [56, 41, 67],
+    [87, 90, 10]
+]
 
-suma = sum(sum(fila) for fila in matriz)
+sumatotal = sum(sum(fila) for fila in matriz)
 
-print(suma)
+print(sumatotal)
